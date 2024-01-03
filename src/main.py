faed78a41f97323c34c915f2d958edfa98a9436a
almaz <almazgimaev@gmail.com>
2024-01-03 19:21:35 +0100

    test

--- src/main.py
+++ src/main.py
@@ -29,6 +29,7 @@ def define_download_method(api, csv_path, images_paths):
 @g.my_app.callback("preview")
 @sly.timeit
 def preview(api: sly.Api, task_id, context, state, app_logger):
+    api.file.get_info_by_path(g.TEAM_ID, g.INPUT_FILE).abc
     images_paths = download_and_preview_table(api, task_id)
     if not g.is_url:
         g.images_size_threshold = define_download_method(api, g.INPUT_FILE, images_paths)
